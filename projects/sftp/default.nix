5e0af534c174f2c4307835ea9995a3961041644b
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2026-02-02 17:36:55 -0800

    Volume support for sftp project

--- projects/sftp/default.nix
+++ projects/sftp/default.nix
@@ -19,15 +19,17 @@
 # If not, see <https://www.gnu.org/licenses/>.
 #
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
     defaults = config.nzc.arion.defaults;
     instance = config.nzc.instance;
+    volumes = instance.storage.volumes;
     secrets = instance.secrets;
 
     exists = {
-      "sftp.public.key" = secrets ? "sftp.public.key";
-      "ssh.public.key"  = secrets ? "ssh.public.key";
+        "volumes" = builtins.length (builtins.attrValues volumes) > 0;
+        "sftp.public.key" = secrets ? "sftp.public.key";
+        "ssh.public.key"  = secrets ? "ssh.public.key";
     };
 
     dockerfile = pkgs.callPackage ./dockerfile {
@@ -67,19 +69,30 @@ in
             ];
         };
 
-        assertions = if (exists."sftp.public.key" && exists."ssh.public.key") then [
+        assertions = [
             {
-                assertion = secrets."sftp.public.key" != secrets."ssh.public.key";
+                assertion = exists.volumes;
+                message = ''
+                    You need to specify storage.volumes for SFTP to serve a purpose.
+                '';
+            }
+            {
+                assertion = if (exists."sftp.public.key" && exists."ssh.public.key") then 
+                    secrets."sftp.public.key" != secrets."ssh.public.key"
+                    else true;
                 message = ''
                     The SSH public key for SFTP and SSH MUST be different:
                     You cannot connect to both at the same time!
                 '';
             }
-        ] else [];
+        ];
 
         project = defaults.project;
+        docker-compose.volumes = defaults.docker-compose.volumes;
         services.sftp.service = defaults.service // {
             build.context = "${dockerfile}";
+            volumes = lib.mapAttrsToList 
+                (_: value: "${toString value}:/home/ssh/${value}") volumes;
             ports = [
                 "${toString instance.network.ports.sftp}:22/tcp"
             ];
